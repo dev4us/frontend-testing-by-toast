1a330c9e54dca972e45131bf1a975ec13b75e3ac
dev4us <bronxofsoul@gmail.com>
2020-01-15 16:07:17 +0900

    # 4. TDD Harder Example

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,26 @@
-export function createCounter() {
+export function createCounter(options = {}) {
+  let val = options.initVal || 0;
+  const min = 0;
+  const max = 5;
+
   return {
     val() {
-
+      return val;
     },
     inc() {
-
+      return val < max ? val++ : false;
     },
     dec() {
-
+      return min < val ? val-- : false;
+    },
+    isMax() {
+      return options.max === val ? true : false;
+    },
+    isMin() {
+      return options.min === val ? true : false;
+    },
+    initVal() {
+      val = 0;
     }
   };
 }
